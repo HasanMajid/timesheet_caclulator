b57658334e8e7938d4a2f3cb71fcc618d4d1ca05
Hasan Majid <hasanmajid3.14@hotmail.com>
2022-11-12 20:03:55 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useRef } from "react";
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(55);
+  const minutes = useRef(0);
+
+  function countUp() {
+    setTimeout(increment, 1000);
+  }
+
+  function increment() {
+    if (count === 60) {
+      minutes.current += 1;
+      setCount(1);
+    } else {
+      setCount(count + 1);
+    }
+  }
+
+  useEffect(() => {
+    countUp();
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{display:'flex', flexDirection:'row', marginLeft: 300, marginRight: 300 }}>
+
+      <div style={styles.div}>
+        <div>Hello World!!</div>
+        <div style={{ fontSize: 30, color: "lightcoral", fontWeight: "bold" }}>
+          {count}
+        </div>
+        <div>{minutes.current} minutes have passed</div>
+      </div>
+
+      {/* <div style={styles.div}>
+        <div>Hello World!!</div>
+        <div style={{ fontSize: 30, color: "lightcoral", fontWeight: "bold" }}>
+          {count}
+        </div>
+        <div>{minutes.current} minutes have passed</div>
+      </div> */}
+
     </div>
   );
-}
+};
+
+const styles = {
+  div: {
+    display: "flex",
+    backgroundColor: "blue",
+    color: "white",
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center",
+    margin: "auto",
+    flexDirection: "column",
+  },
+};
 
 export default App;
